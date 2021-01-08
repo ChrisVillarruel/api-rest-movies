c48586ccf1ca78e781420ec2c48157ad32b8a05a
christian <chris_villarruel@outlook.com>
2021-01-07 19:46:20 -0600

    Se agrego el query de la creación de la tabla

--- Movies.sql
+++ Movies.sql
@@ -7,3 +7,10 @@ create table tbl_movie_category(
 );CREATE DATABASE `moviesdb` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci */ /*!80016 DEFAULT ENCRYPTION='N' */;
 
 
+create table tbl_classification(
+    classification_id int unsigned auto_increment,
+    classification_name varchar(3) not null,
+    classification_desc varchar(100) default "Sin descripción",
+    primary key(classification_id),
+    unique u_category(classification_desc )
+);CREATE DATABASE `moviesdb` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci */ /*!80016 DEFAULT ENCRYPTION='N' */;

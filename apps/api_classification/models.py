6fabccc07f2bb1a86bff4fb3cd9eeff6fa3e3a89
christian <chris_villarruel@outlook.com>
2021-01-07 20:52:43 -0600

    Se creo la entidad Classification

--- apps/api_classification/models.py
+++ apps/api_classification/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
-# Create your models here.
+
+class Classification(models.Model):
+    classification_id = models.AutoField(primary_key=True)
+    classification_name = models.CharField(max_length=3)
+    classification_desc = models.CharField(unique=True, max_length=100, blank=True, null=True, default='Sin descripción')
+
+    class Meta:
+        managed = False
+        db_table = 'tbl_classification'
+        verbose_name = 'Clasificación'
+        verbose_name_plural = 'Clasificaciones'
+        ordering = ['classification_id']
+
+    def __str__(self):
+        return f'{self.classification_id}: {self.classification_name.upper()}, {self.classification_desc}'

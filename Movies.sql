7919ab497ef61d6c2de0d819f7e1ac95bf84545b
christian <chris_villarruel@outlook.com>
2021-01-07 21:09:58 -0600

    Se agrego el query de la creacion de la tabla movies

--- Movies.sql
+++ Movies.sql
@@ -14,3 +14,17 @@ create table tbl_classification(
     primary key(classification_id),
     unique u_category(classification_desc )
 );CREATE DATABASE `moviesdb` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci */ /*!80016 DEFAULT ENCRYPTION='N' */;
+
+
+create table tbl_movies(
+    movie_id int unsigned auto_increment,
+    name_movie varchar(100) not null,
+    lauch_year varchar(4) not null,
+    sinopsis varchar(255) not null,
+    duration varchar(20) not null,
+    category_id int(3) unsigned not null,
+    classification_id int(3) unsigned not null,
+    primary key(movie_id),
+    CONSTRAINT FK_category_id FOREIGN KEY (category_id) REFERENCES tbl_movie_category(category_id),
+    CONSTRAINT FK_classification_id FOREIGN KEY (classification_id) REFERENCES tbl_classification(classification_id)
+);CREATE DATABASE `moviesdb` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci */ /*!80016 DEFAULT ENCRYPTION='N' */;

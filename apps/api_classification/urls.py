4ca3961fc5d5371f79b275927e7eb0a40ce15104
christian <chris_villarruel@outlook.com>
2021-01-08 21:01:00 -0600

    Se creo la ruta del listado y/o creación de una clasificación, ademas de la ruta detalle, actualización y eliminación de una clasificación

---
+++ apps/api_classification/urls.py
@@ -0,0 +1,7 @@
+from django.urls import path
+from .api import *
+
+urlpatterns = [
+    path('api/', ClassificationAPI.as_view(), name='list'),
+    path('api/<int:pk>', ClassificationDetailAPI.as_view(), name='detail'),
+]

13827c5f3055cd39eb79e949430d8c46f00f2f09
christian <chris_villarruel@outlook.com>
2021-01-09 12:30:46 -0600

    Se creo la ruta global de la api category_movies

--- config/urls.py
+++ config/urls.py
@@ -19,4 +19,5 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('classification/', include('apps.api_classification.urls')),
+    path('category/', include('apps.api_movie_category.urls')),
 ]

da51e7f34850c6c3533543879e5329b2d60e90b6
christian <chris_villarruel@outlook.com>
2021-01-13 10:43:44 -0600

    Se modifico la estructura de errores de respuesta en json

--- apps/api_classification/api.py
+++ apps/api_classification/api.py
@@ -8,47 +8,45 @@ from .models import Classification
 from .serializers import ClassificationSerializer, ClassificationDetailSerializer
 
 
-def msg_error(error, detail=None):
+def msg_error(message, status, code, detail=None):
+    if detail is None:
+        detail = 'Error no detallado'
+
     msg_error = {
-        'error': error,
-        'detail': detail,
-        'date': datetime.now().strftime("%A, %d. %B %Y %I:%M %p")
+        'error': {
+            'message': message,
+            'status': status,
+            'code': code,
+            'date': datetime.now().strftime("%d-%m-%Y"),
+            'details': detail
+        }
     }
     return msg_error
 
 class ClassificationAPI(APIView):
 
-    # list Classification
     def get(self, request):
         query_set = Classification.objects.all().values('classification_id', 'classification_name')
         serializers = ClassificationSerializer(instance=query_set, many=True)
         return Response(serializers.data, status=status.HTTP_200_OK)
 
-    # create Classification
     def post(self, request):
         serializer = ClassificationSerializer(data=request.data)
 
-        """ validaciones """
         if serializer.is_valid():
             classification_instance = serializer.save()
-            msg_success = {
-                'success': f'Clasificación {classification_instance.classification_name} creada'
-            }
+            msg_success = {'success': f'La Clasificación {classification_instance.classification_name} creada'}
             return Response(msg_success, status=status.HTTP_201_CREATED)
 
-        """ Error de validaciones """
-        error = msg_error('Error de validación', serializer.errors)
+        error = msg_error('Error de validación', 'BAD_REQUEST', 400, serializer.errors)
         return Response(error, status=status.HTTP_400_BAD_REQUEST)
-
 
 class ClassificationDetailAPI(APIView):
 
-    # get all Classification
     def get_query(self, pk=None):
         query_set = Classification.objects.filter(pk=pk).first()
         return query_set
 
-    # detail Classification
     def get(self, request, pk=None):
         query_set = self.get_query(pk)
 
@@ -56,30 +54,25 @@ class ClassificationDetailAPI(APIView):
             serializer = ClassificationDetailSerializer(query_set)
             return Response(serializer.data, status=status.HTTP_200_OK)
 
-        error = msg_error('La Clasificación que usted ingreso no existe')
-        return Response(error, status=status.HTTP_400_BAD_REQUEST)
+        error = msg_error('Clasificación no encontrada', 'NOT_FOUND', 404)
+        return Response(error, status=status.HTTP_404_NOT_FOUND)
 
-    # update Classification
     def put(self, request, pk=None):
         query_set = self.get_query(pk)
 
         if query_set is not None:
             serializer = ClassificationSerializer(instance=query_set, data=request.data)
 
-            # validamos serializador
             if serializer.is_valid():
                 classification_instance = serializer.save()
-                msg_success = {
-                    'success': f'Clasificación {classification_instance.classification_name} actualizada'
-                }
+                msg_success = {'success': f'La Clasificación {classification_instance.classification_name} actualizada'}
                 return Response(msg_success, status=status.HTTP_200_OK)
 
-            # Error de validación
-            error = msg_error('Error de validación', serializer.errors)
+            error = msg_error('Error de validación', 'BAD_REQUEST', 400, serializer.errors)
             return Response(error, status=status.HTTP_400_BAD_REQUEST)
 
-        error = msg_error('La Clasificación que usted ingreso no existe')
-        return Response(error, status=status.HTTP_400_BAD_REQUEST)
+        error = msg_error('Clasificación no encontrada', 'NOT_FOUND', 404)
+        return Response(error, status=status.HTTP_404_NOT_FOUND)
 
     # delete Classification
     def delete(self, request, pk=None):
@@ -87,8 +80,8 @@ class ClassificationDetailAPI(APIView):
 
         if query_set is not None:
             query_set.delete()
-            msg_success = {'success': f'Clasificación {query_set.classification_name} eliminada'}
+            msg_success = {'success': f'La Clasificación {query_set.classification_name} eliminada'}
             return Response(msg_success, status=status.HTTP_200_OK)
 
-        error = msg_error('La Clasificación que ha ingresado no existe')
-        return Response(error, status=status.HTTP_400_BAD_REQUEST)
+        error = msg_error('Clasificación no encontrada', 'NOT_FOUND', 404)
+        return Response(error, status=status.HTTP_404_NOT_FOUND)

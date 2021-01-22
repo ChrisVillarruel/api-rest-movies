43baeeb8329013f4dfd4050571641f515e341e32
christian <chris_villarruel@outlook.com>
2021-01-21 21:55:36 -0600

    Se optimizo el codigo

--- apps/api_movies/api.py
+++ apps/api_movies/api.py
@@ -2,28 +2,13 @@ from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
 from rest_framework.pagination import PageNumberPagination
-from datetime import datetime
+
 
 from .models import Movies
+from apps.msg_error import msg_error
 from .serializers import MoviesSerializer, MoviesDetailSerializer
 
 # mostrar error de validación
-
-
-def msg_error(message, status, code, detail=None):
-    if detail is None:
-        detail = 'Error no detallado'
-
-    msg_error = {
-        'error': {
-            'message': message,
-            'status': status,
-            'code': code,
-            'date': datetime.now().strftime("%d-%m-%Y"),
-            'details': detail
-        }
-    }
-    return msg_error
 
 
 class MoviesAPI(APIView):

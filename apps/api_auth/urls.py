e568d9a9e8094aa52d02830b45e8258658744c6b
ChrisVillarruel <givc980909@gmail.com>
2021-03-12 11:05:18 -0600

    Se elimino la app api_auth

--- apps/api_auth/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from .api import *
-
-urlpatterns = [
-    path('register/', RegisterView.as_view(), name='register'),
-    # path('api/<int:pk>', ClassificationDetailAPI.as_view(), name='detail'),
-]

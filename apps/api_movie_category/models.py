17582bef9fdd8b79ac03e9bbc7bb99c81a6478c8
ChrisVillarruel <givc980909@gmail.com>
2021-03-13 11:19:57 -0600

    Se modifico el model MovieCategory. Ahora este modelo hereda de baseModel

--- apps/api_movie_category/models.py
+++ apps/api_movie_category/models.py
@@ -1,18 +1,22 @@
+# Modulos nativos de django
 from django.db import models
 
+# Modulos locales
+from apps.base.models import BaseModel
 
-# class MovieCategory(models.Model):
-#     pass
-# category_id = models.AutoField(primary_key=True)
-# category_name = models.CharField(unique=True, max_length=100)
-# category_desc = models.CharField(max_length=250, blank=True, null=True, default='Sin descripción')
 
-# class Meta:
-#     # managed = False
-#     db_table = 'tbl_movie_category'
-#     verbose_name = 'Categoria'
-#     verbose_name_plural = 'Categorias'
-#     ordering = ['category_id']
+class MovieCategory(BaseModel):
+    # Modelo que hereda de Base model. Cada pelicula debe de tener una categoria
 
-# def __str__(self):
-#     return f'{self.category_name}'
+    category_id = models.AutoField(primary_key=True)
+    category_name = models.CharField(unique=True, max_length=100)
+    category_desc = models.CharField(max_length=250, null=False)
+
+    class Meta:
+        db_table = 'tbl_movie_category'
+        verbose_name = 'Categoria'
+        verbose_name_plural = 'Categorias'
+        ordering = ['category_id']
+
+    def __str__(self):
+        return f'{self.category_name}'

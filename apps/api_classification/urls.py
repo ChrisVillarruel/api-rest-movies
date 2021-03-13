3e551981d675c9ea02c83c145c050c0c37b01f6b
ChrisVillarruel <givc980909@gmail.com>
2021-03-13 11:14:10 -0600

    Se elimino del proyecto el modulo urls de classfication, pues fue remplazado por el modulo routers

--- apps/api_classification/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from .api import *
-
-urlpatterns = [
-    path('api/', ClassificationAPI.as_view(), name='create'),
-    path('api/<int:pk>', ClassificationDetailAPI.as_view(), name='detail'),
-]

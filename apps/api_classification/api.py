ccad373c41b379006c0d79369edde2bbe10dfad7
ChrisVillarruel <givc980909@gmail.com>
2021-03-13 11:06:14 -0600

    Se eliminaron las vistas ClassificationDetailAPIView y ClassificationAPIView y se añadio como unica vista ClassificationViewSet

--- apps/api_classification/api.py
+++ apps/api_classification/api.py
@@ -8,72 +8,9 @@ from rest_framework import status
 from datetime import datetime
 
 # Modulos Locales
-from .serializers import ClassificationSerializer, ClassificationDetailSerializer
+from .serializers import ClassificationSerializer
 from apps.metodosExternos import msg_error, msg_success
 from .models import Classification
-
-class ClassificationAPI(APIView):
-
-    def get(self, request):
-        query_set = Classification.objects.all().values('classification_id', 'classification_name', 'classification_desc')
-        serializers = ClassificationSerializer(instance=query_set, many=True)
-        return Response(serializers.data, status=status.HTTP_200_OK)
-
-    def post(self, request):
-        serializer = ClassificationSerializer(data=request.data)
-
-        if serializer.is_valid():
-            serializer.save()
-            msg_success = {'success': 'Clasificación creada exitosamente'}
-            return Response(msg_success, status=status.HTTP_201_CREATED)
-
-        error = msg_error('Error de validación', 'BAD_REQUEST', 400, serializer.errors)
-        return Response(error, status=status.HTTP_400_BAD_REQUEST)
-
-class ClassificationDetailAPI(APIView):
-
-    def get_query(self, pk=None):
-        query_set = Classification.objects.filter(pk=pk).first()
-        return query_set
-
-    def get(self, request, pk=None):
-        query_set = self.get_query(pk)
-
-        if query_set is not None:
-            serializer = ClassificationDetailSerializer(query_set)
-            return Response(serializer.data, status=status.HTTP_200_OK)
-
-        error = msg_error('Clasificación no encontrada', 'NOT_FOUND', 404)
-        return Response(error, status=status.HTTP_404_NOT_FOUND)
-
-    def put(self, request, pk=None):
-        query_set = self.get_query(pk)
-
-        if query_set is not None:
-            serializer = ClassificationSerializer(instance=query_set, data=request.data)
-
-            if serializer.is_valid():
-                classification_instance = serializer.save()
-                msg_success = {'success': 'Clasificación actualizada exitosamente'}
-                return Response(msg_success, status=status.HTTP_200_OK)
-
-            error = msg_error('Error de validación', 'BAD_REQUEST', 400, serializer.errors)
-            return Response(error, status=status.HTTP_400_BAD_REQUEST)
-
-        error = msg_error('Clasificación no encontrada', 'NOT_FOUND', 404)
-        return Response(error, status=status.HTTP_404_NOT_FOUND)
-
-    # delete Classification
-    def delete(self, request, pk=None):
-        query_set = self.get_query(pk)
-
-        if query_set is not None:
-            query_set.delete()
-            msg_success = {'success': f'Clasificación eliminada exitosamente'}
-            return Response(msg_success, status=status.HTTP_200_OK)
-
-        error = msg_error('Clasificación no encontrada', 'NOT_FOUND', 404)
-        return Response(error, status=status.HTTP_404_NOT_FOUND)
 
 
 class ClassificationViewSet(ModelViewSet):
@@ -83,10 +20,10 @@ class ClassificationViewSet(ModelViewSet):
         # Si pk es None, retornamos un listado. De lo contrario retornamos un objeto
 
         if pk is None:
-            return self.get_serializer().Meta.model.objects.filter().values(
+            return self.get_serializer().Meta.model.objects.filter(state=True).values(
                 'classification_id', 'classification_name', 'classification_desc')
 
-        return self.get_serializer().Meta.model.filter(classification_id=pk, state=True).first()
+        return self.get_serializer().Meta.model.objects.filter(classification_id=pk, state=True).first()
 
     def create(self, request):
         # Creación de una Clasificación
@@ -101,10 +38,10 @@ class ClassificationViewSet(ModelViewSet):
     def update(self, request, pk=None):
         # Actualización parcial de una Clasificación
 
-        classification = self.get_queryset(pk)
-        if classification is not None:
+        queryset = self.get_queryset(pk)
+        if queryset:
 
-            serializer = self.serializer_class(instance=classification, data=request.data, partial=True)
+            serializer = self.serializer_class(queryset, data=request.data, partial=True)
             serializer.is_valid(raise_exception=True)
             serializer.save()
 

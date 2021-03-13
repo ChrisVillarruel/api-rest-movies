54a6b79489f23872b422a55abc143eea624278d6
ChrisVillarruel <givc980909@gmail.com>
2021-03-13 11:37:51 -0600

    Se actualizo el serializador

--- apps/api_movie_category/serializers.py
+++ apps/api_movie_category/serializers.py
@@ -1,45 +1,46 @@
-# from rest_framework import serializers
+from rest_framework import serializers
 
-# from .models import MovieCategory
+from .models import MovieCategory
 
 
-# class CategorySerializers(serializers.ModelSerializer):
-#     category_name = serializers.CharField(max_length=100, min_length=5)
-#     category_desc = serializers.CharField(max_length=250, min_length=5, allow_blank=True)
+class MovieCategorySerializers(serializers.ModelSerializer):
+    category_name = serializers.CharField(max_length=100, min_length=5)
+    category_desc = serializers.CharField(max_length=250)
 
-#     class Meta:
-#         model = MovieCategory
-#         fields = ['category_id', 'category_name', 'category_desc']
+    class Meta:
+        model = MovieCategory
+        fields = ['category_id', 'category_name', 'category_desc']
+        exclude = ('state', 'created_at', 'updated_at', 'deleted_at')
 
-#     def validate_category_name(self, value):
-#         category_name = value
+    def validate_category_name(self, value):
+        category_name = value
 
-#         if category_name.isnumeric():
-#             raise serializers.ValidationError(
-#                 'Asegurese que este campo contenga unicamente caracteres alfabeticos')
+        if category_name.isnumeric():
+            raise serializers.ValidationError(
+                'Asegurese que este campo contenga unicamente caracteres alfabeticos')
 
-#         if MovieCategory.objects.filter(category_name=category_name).exists():
-#             raise serializers.ValidationError(
-#                 f'Ya existe una categoria con el nombre {category_name.upper()}')
+        if MovieCategory.objects.filter(category_name=category_name).exists():
+            raise serializers.ValidationError(
+                f'Ya existe una categoria con el nombre {category_name.upper()}')
 
-#         return value
+        return value
 
-#     def validate_category_desc(self, value):
-#         category_desc = value
+    def validate_category_desc(self, value):
+        category_desc = value
 
-#         if category_desc.isnumeric():
-#             raise serializers.ValidationError(
-#                 'Asegurese que este campo contenga unicamente caracteres alfabeticos')
-#         return value
+        if category_desc.isnumeric():
+            raise serializers.ValidationError(
+                'Asegurese que este campo contenga unicamente caracteres alfabeticos')
+        return value
 
-#     def validate(self, data):
-#         return data
+    def validate(self, data):
+        return data
 
-#     def create(self, validate_data):
-#         return MovieCategory.objects.create(**validate_data)
+    def create(self, validate_data):
+        return MovieCategory.objects.create(**validate_data)
 
-#     def update(self, instance, validate_data):
-#         instance.category_name = validate_data.get('category_name', instance.category_name)
-#         instance.category_desc = validate_data.get('category_desc', instance.category_desc)
-#         instance.save()
-#         return instance
+    def update(self, instance, validate_data):
+        instance.category_name = validate_data.get('category_name', instance.category_name)
+        instance.category_desc = validate_data.get('category_desc', instance.category_desc)
+        instance.save()
+        return instance

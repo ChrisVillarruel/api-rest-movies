ee152289155ede1fb6b49a71d58f5820b4f1bc55
ChrisVillarruel <givc980909@gmail.com>
2021-03-12 20:49:43 -0600

    Se añadio el modulo routers como una forma de acceder al viewSet

--- config/urls.py
+++ config/urls.py
@@ -3,7 +3,7 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('classifications/', include('apps.api_classification.urls')),
+    path('classifications/', include('apps.api_classification.routers')),
     path('categories/', include('apps.api_movie_category.urls')),
     path('movies/', include('apps.api_movies.urls')),
 ]

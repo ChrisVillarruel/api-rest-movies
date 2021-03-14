ed6879d326eab37169812e13a13ed65f0de04d01
ChrisVillarruel <givc980909@gmail.com>
2021-03-14 11:41:36 -0600

    Se añadio la vista viewset con el objetivo de sustituir las vistas List y detail

--- apps/api_movie_category/api.py
+++ apps/api_movie_category/api.py
@@ -5,95 +5,50 @@ from rest_framework.response import Response
 from rest_framework import status
 
 # from .serializers import CategorySerializers
-from apps.metodosExternos import msg_error, msg_success
+from apps.metodosExternos import msg_error, resource_created, resource_updated, resource_destroy, not_found
 from .serializers import MovieCategorySerializers
 from .models import MovieCategory
-
-
-# class CategoryMoviesAPI(APIView):
-
-#     def get(self, request):
-#         query_set = MovieCategory.objects.all()
-#         serializers = CategorySerializers(instance=query_set, many=True)
-#         return Response(serializers.data, status=status.HTTP_200_OK)
-
-#     # create
-#     def post(self, request):
-#         json_data = request.data
-#         serializer = CategorySerializers(data=json_data)
-
-#         # validaciones
-#         if serializer.is_valid():
-#             category_instance = serializer.save()
-#             msg_success = {
-#                 'success': f'La Categoria {category_instance.category_name} creada exitosamente'}
-#             return Response(msg_success, status=status.HTTP_201_CREATED)
-
-#         # error de validaciones
-#         error = msg_error('Error de validación', 'BAD_REQUEST', 400, serializer.errors)
-#         return Response(error, status=status.HTTP_400_BAD_REQUEST)
-
-
-# class CategoryMoviesDetailAPI(APIView):
-
-#     # Para evitar el uso de excepciones se creo este metodo
-#     def get_query(self, pk=None):
-#         query_set = MovieCategory.objects.filter(pk=pk).first()
-#         return query_set
-
-#     # detallar categoria
-#     def get(self, request, pk=None):
-#         query_set = self.get_query(pk)
-
-#         if query_set is not None:
-#             serializer = CategorySerializers(instance=query_set)
-#             return Response(serializer.data, status=status.HTTP_200_OK)
-
-#         error = msg_error('Categoria no encontrada', 'NOT_FOUND', 404)
-#         return Response(error, status=status.HTTP_404_NOT_FOUND)
-
-#     # actualizar categoria
-#     def put(self, request, pk=None):
-#         query_set = self.get_query(pk)
-
-#         if query_set is not None:
-#             serializer = CategorySerializers(instance=query_set, data=request.data)
-
-#             # validaciones
-#             if serializer.is_valid():
-#                 category_instance = serializer.save()
-#                 msg_success = {
-#                     'success': f'La Categoria {category_instance.category_name} actualizada exitosamente'}
-#                 return Response(msg_success, status=status.HTTP_200_OK)
-
-#             # Error de validación
-#             error = msg_error('Error de validación', 'BAD_REQUEST', 400, serializer.errors)
-#             return Response(error, status=status.HTTP_400_BAD_REQUEST)
-
-#         # categoria no encontrada
-#         error = msg_error('Categoria no encontrada', 'NOT_FOUND', 404)
-#         return Response(error, status=status.HTTP_404_NOT_FOUND)
-
-#     # eliminar Categoria
-#     def delete(self, request, pk=None):
-#         query_set = self.get_query(pk)
-
-#         if query_set is not None:
-#             query_set.delete()
-#             msg_success = {
-#                 'success': f'La Categoria {query_set.category_name} eliminada exitosamente'}
-#             return Response(msg_success, status=status.HTTP_200_OK)
-
-#         # categoria no encontrada
-#         error = msg_error('Categoria no encontrada', 'NOT_FOUND', 404)
-#         return Response(error, status=status.HTTP_404_NOT_FOUND)
 
 
 class MovieCategoryViewSet(ModelViewSet):
     serializer_class = MovieCategorySerializers
 
     def get_queryset(self, pk=None):
+        # Si pk es None, retornamos un listado. De lo contrario retornamos un objeto
+
         if pk is None:
             return self.get_serializer().Meta.model.objects.filter(state=True)
 
-        return self.get_serializer().Meta.model.objects.filter(category_id=pk, state=True).first()
+        return self.get_serializer().Meta.model.objects.filter(category_id=pk, state=True)
+
+    def create(self, request):
+        # Metodo encargado de crear una categoria
+
+        serializer = self.serializer_class(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+
+        return Response(resource_created(), status=status.HTTP_201_CREATED)
+
+    def update(self, request, pk=None):
+        # Metodo encargado de actualizar una categoria
+
+        queryset = self.get_queryset(pk)
+        if queryset:
+            serializer = self.serializer_class(queryset, data=request.data, partial=True)
+            serializer.is_valid(raise_exception=True)
+            serializer.save()
+
+            return Response(resource_updated(), status=status.HTTP_200_OK)
+        return Response(not_found(), status=status.HTTP_404_NOT_FOUND)
+
+    def destroy(self, request, pk=None):
+        # Metodo encargado de eliminar de manera logica una categoria
+
+        queryset = self.get_queryset(pk)
+        if queryset:
+            queryset.state = False
+            queryset.save()
+
+            return Response(resource_destroy(), status=status.HTTP_200_OK)
+        return Response(not_found(), status=status.HTTP_404_NOT_FOUND)

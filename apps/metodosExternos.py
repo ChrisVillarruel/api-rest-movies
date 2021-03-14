c86c25ed7e635c3ebac5387dbb57d188a70cc703
ChrisVillarruel <givc980909@gmail.com>
2021-03-14 11:44:49 -0600

    Se añadieron metodos que retornan un diccionario como respuesta al realizar una acción

--- apps/metodosExternos.py
+++ apps/metodosExternos.py
@@ -1,25 +1,42 @@
 from datetime import datetime
 
 
-def msg_error(message, status, code, detail=None):
+def msg_error(message, code, detail=None):
     if detail is None:
         detail = 'Error no detallado'
 
     msg_error = {
         'error': {
             'message': message,
-            'status': status,
             'code': code,
-            'date': datetime.now().strftime("%d-%m-%Y"),
             'details': detail
         }
     }
     return msg_error
 
 
-def msg_success(message, code):
-    msg_success = {
-        'message': message,
-        'code': code
+def base_resource(message, code):
+    return {
+        'detail': message,
+        'status_code': code
     }
-    return msg_success
+
+
+def resource_created():
+    created = base_resource(message='Recurso Creado.', code=201)
+    return created
+
+
+def resource_updated():
+    update = base_resource(message='Recurso Actualizado.', code=200)
+    return update
+
+
+def resource_destroy():
+    destroy = base_resource(message='Recurso Eliminado.', code=200)
+    return destroy
+
+
+def not_found():
+    not_found = base_resource(message='No encontrado.', code=404)
+    return not_found

b7fe790d8352497f29968f6533f28cb6deb6cdce
ChrisVillarruel <givc980909@gmail.com>
2021-03-14 19:34:43 -0600

    Se creo el modulo routers que sustituira al modulo urls

--- apps/api_movies/urls.py
+++
@@ -1,7 +0,0 @@
-# from django.urls import path
-# from .api import *
-
-# urlpatterns = [
-#     path('api/', MoviesAPI.as_view(), name='list'),
-#     path('api/<int:pk>', MoviesDetailAPI.as_view(), name='detail'),
-# ]
